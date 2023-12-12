5f66e55810da5e04e503ba55b2ea17c7786b2ed9
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-12-12 15:21:14 -0700

    Adding notes from jupyter

--- Lesson2/Amcelinearregression.py
+++ Lesson2/Amcelinearregression.py
@@ -1,7 +1,31 @@
+# because we are taking information from a url, using urlretrieval will allow us to pull a url
+# pandas to read the csv
+# numpy to calucuate data
 from urllib.request import urlretrieve
 import pandas as pd
+import numpy as np
+# to save data
+import jovian
+# to visualize the data
+import plotly.express as px
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
-urlretrieve(medical_charges_url, 'medical.csv')
-medical_df = pd.read_csv('medical.csv')
-print(medical_df)
+
+# start with getting the data from the url
+medicaldataurl = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
+
+# we'll want to extract the data from the url, and make it into a CSV so pandas can read it
+urlretrieve(medicaldataurl, 'acmemedical.csv')
+
+# now that we have the amcemedical data saved as a csv, well need to turn it into a df using pandas
+acemedicaldf = pd.read_csv('acmemedical.csv')
+
+# to display the acme dataframe
+print(acemedicaldf)
+
+# we can check other things about our data, to make sure its nice and clean. Infor is to show the types of data, and know if there is any missing data
+# decribe gives us aggregate insights about the data
+acemedicaldf.info()
+acemedicaldf.describe()
