c157c810bf414db1a1dbc452eb2b59dcff3b4566
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-12-12 14:33:52 -0700

    Start of amce lesson LR

---
+++ Lesson2/Amcelinearregression.py
@@ -0,0 +1,7 @@
+from urllib.request import urlretrieve
+import pandas as pd
+
+medical_charges_url = 'https://raw.githubusercontent.com/JovianML/opendatasets/master/data/medical-charges.csv'
+urlretrieve(medical_charges_url, 'medical.csv')
+medical_df = pd.read_csv('medical.csv')
+print(medical_df)
