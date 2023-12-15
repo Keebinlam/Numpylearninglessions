6b040353afabf4c2ad693e98d81abf2ec2cf63c2
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-12-15 12:04:55 -0700

    adding sgr, and catorgizing

--- Lesson2/Amcelinearregression.py
+++ Lesson2/Amcelinearregression.py
@@ -129,16 +129,19 @@ print(correlation_matrix)
 sns.heatmap(correlation_matrix, cmap='Reds', annot=True)
 plt.title('Correlation Matrix')
 
-#do not need to use a Linear regression Single Feature, or Loss/Cost Function, just use scikit learn
-#in geometri, y = mx +b. (m being the slope, and b being the interceptor: or the point that the slope starts at Y =0.
-#but in ML, m = w which means weigh, and b is bias. This is just a linear equation
+# do not need to use a Linear regression Single Feature, or Loss/Cost Function, just use scikit learn
+# in geometri, y = mx +b. (m being the slope, and b being the interceptor: or the point that the slope starts at Y =0.
+# but in ML, m = w which means weigh, and b is bias. This is just a linear equation
 # We need a way to measure numerically how well the line fits the points.
 # Once the "measure of fit" has been computed, we need a way to modify w and b to improve the the fit.
 # If we can solve the above problems, it should be possible for a computer to determine w and b for the best fit line, starting from a random guess.
 
 non_smoker_df = acemedicaldf[acemedicaldf.smoker == 'no']
+
+
 def test(w, x, b):
-   return w * x + b
+    return w * x + b
+
 
 x = non_smoker_df.age
 w = 100
@@ -146,52 +149,259 @@ b = 200
 
 estimated_charges = test(w, x, b)
 
-plt.plot(x, estimated_charges, 'r-o');
-plt.xlabel('Age');
-plt.ylabel('Estimated Charges');
+plt.plot(x, estimated_charges, 'r-o')
+plt.xlabel('Age')
+plt.ylabel('Estimated Charges')
 
 target = non_smoker_df.charges
 
-plt.plot(x, estimated_charges, 'r', alpha=0.9);
-plt.scatter(x, target, s=8,alpha=0.8);
-plt.xlabel('Age');
+plt.plot(x, estimated_charges, 'r', alpha=0.9)
+plt.scatter(x, target, s=8, alpha=0.8)
+plt.xlabel('Age')
 plt.ylabel('Charges')
-plt.legend(['Estimate', 'Actual']);
+plt.legend(['Estimate', 'Actual'])
 
 x = non_smoker_df.age
 target = non_smoker_df.charges
 estimated_charges = test(w, x, b)
 
-#to summarize what just happen, I created a graph that plots the acme data, as well as a linear function (not actual linear line from correlation coeffiecnt
-#then created a function showing that takes in x and b paremeters, into a linear equation. The purpose of this is to plug in different numbers, and see how the line moves
-#the goal is to get the link to match as close to the general sloped line of the acme data
-#now we need to know how to measure how accurate our line is. This is important in ML because we can measure how close or not close we are 
+# to summarize what just happen, I created a graph that plots the acme data, as well as a linear function (not actual linear line from correlation coeffiecnt
+# then created a function showing that takes in x and b paremeters, into a linear equation. The purpose of this is to plug in different numbers, and see how the line moves
+# the goal is to get the link to match as close to the general sloped line of the acme data
+# now we need to know how to measure how accurate our line is. This is important in ML because we can measure how close or not close we are
 
-#root mean square error 
+# root mean square error
+
+
 def rmse(targets, predictions):
     return np.sqrt(np.mean(np.square(targets - predictions)))
+
 
 predicted = test(x, w, b)
 
 rmse(target, predicted)
 
+
 def try_parameters(w, b):
     ages = non_smoker_df.age
     target = non_smoker_df.charges
     predictions = test(x, w, b)
-    
-    plt.plot(x, predictions, 'r', alpha=0.9);
-    plt.scatter(x, target, s=8,alpha=0.8);
-    plt.xlabel('Age');
+
+    plt.plot(x, predictions, 'r', alpha=0.9)
+    plt.scatter(x, target, s=8, alpha=0.8)
+    plt.xlabel('Age')
     plt.ylabel('Charges')
-    plt.legend(['Prediction', 'Actual']);
-    
+    plt.legend(['Prediction', 'Actual'])
+
     loss = rmse(target, predictions)
     print("RMSE Loss: ", loss)
 
     try_parameters(220, 50)
 
-    #in practice, I do not need to calculate a line, and measure the amount of residual of that line compared to the line of best. 
-#the linear equeation function is used to plot a line, but using Ordinary Least Squares and Stochastic gradient descent will help adjust the slope and intercept
-#rmse shows how far or close we are to the line of best fit
-#the Ordinary Least Squares and Stochastic gradient descent are used to reduce the value of RMSE, indicating the accuracy of the line plots
+    # in practice, I do not need to calculate a line, and measure the amount of residual of that line compared to the line of best.
+# the linear equeation function is used to plot a line, but using Ordinary Least Squares and Stochastic gradient descent will help adjust the slope and intercept
+# rmse shows how far or close we are to the line of best fit
+# the Ordinary Least Squares and Stochastic gradient descent are used to reduce the value of RMSE, indicating the accuracy of the line plots
+
+    from sklearn.linear_model import SGDRegressor
+
+
+# Filter the data for smokers
+smoker_df = acemedical_df[acemedical_df['smoker'] == 'yes']
+
+# Define the inputs (age) and targets (charges)
+inputs = smoker_df[['age']].values
+targets = smoker_df['charges'].values
+
+# Create and train the model
+sgdr = SGDRegressor()
+sgdr.fit(inputs, targets)
+
+# Make predictions
+predictions = sgdr.predict(inputs)
+
+# Plotting the data and the regression line
+plt.figure(figsize=(10, 6))
+plt.scatter(smoker_df['age'], smoker_df['charges'],
+            color='blue', alpha=0.5, label='Actual charges')
+plt.plot(smoker_df['age'], predictions,
+         color='red', label='SGD Regression Line')
+plt.xlabel('Age')
+plt.ylabel('Charges')
+plt.title('SGD Regression for Charges vs. Age of Smokers')
+plt.legend()
+plt.show()
+
+
+#create input and Target (to fit in parameters)
+#create and train a model
+#generate preditctions
+#compute loss to evaluate the model
+#By automating these functions, scikit-learn's LinearRegression allows users to focus on interpreting results and applying them to their specific problems, without getting bogged down in the complexities of the underlying mathematical processes.
+#from reading the sci kit learn notes, essentially the library used in scikit learn. The LinearRegression model automatically calculates the coefficients (parameters) for the linear equation that best fits the given data
+ #It also computes the intercept (the value of the target when all predictors are zero), which is a crucial part of the linear equation.
+
+inputs = non_smoker_df[['age']] #here we are making the conversion to two columns, to make the data fit correctly. column 2 is just counting
+targets = non_smoker_df.charges #the model needs input and target. x and y, while weight and bias will be calculated by the scikitlearn library
+lrm = LinearRegression() #inside the LinearRegression(), we need (input, and target)
+
+lrm.fit(inputs, target) #we are putting in the age, and charges to the model
+#this also codes the loss 
+#this line of code does everything for you
+
+lrm.fit(inputs, target) #we are putting in the age, and charges to the model
+#this also codes the loss 
+#this line of code does everything for you
+
+b = lrm.intercept_ #this is the bias (y=mx=b)
+
+predictions = lrm.predict(inputs) #we do this to overwrite the first prediction variable 
+
+rmse(targets, predictions)
+
+try_parameters(lrm.coef_, lrm.intercept_) #putting in the model into a plot 
+
+#lets try the SGDRegressor class
+sgr = SGDRegressor()
+sgr.fit(inputs, targets)
+
+predictions = sgr.predict(inputs)
+
+sgr.coef_
+
+sgr.intercept_
+
+try_parameters(sgr.coef_, sgr.intercept_)
+
+#now lets try for smokers
+smoker_df = acemedicaldf[acemedicaldf.smoker == 'yes']
+targets2 = smoker_df.charges
+inputs2 = smoker_df[['age']]
+lrm2 = LinearRegression()
+
+lrm2.fit(inputs2, targets2)
+
+
+predictions2 = lrm2.predict(inputs2)
+
+lrm2.coef_
+
+lrm2.intercept_
+
+targets2 = smoker_df.charges
+
+def try_parameters(w, b): #i just realize that this whole function is to plot out data, and also plot the model, and also spit back out the residual
+    ages = smoker_df.age
+    target = smoker_df.charges
+
+    plt.plot(smoker_df.age, redictions2, 'r', alpha=0.9);
+    plt.scatter(smoker_df.age, targets2, s=8,alpha=0.8);
+    plt.xlabel('Age');
+    plt.ylabel('Charges')
+    plt.legend(['Prediction', 'Actual']);
+
+try_parameters(lrm2.coef_, lrm2.intercept_)
+
+ plt.scatter(smoker_df.age, smoker_df.charge)
+
+#now lets try for smokers using the sgr
+smoker_df = acemedicaldf[acemedicaldf.smoker == 'yes']
+targets3 = smoker_df.charges
+inputs3 = smoker_df[['age']]
+x2 = smoker_df.age
+sgdr = SGDRegressor()
+
+sgdr.fit(inputs3, targets3)
+
+sgdr.coef_
+
+sgdr.intercept_
+
+predictions3 = sgdr.predict(inputs3)
+
+def try_parameters3(w, b): #i just realize that this whole function is to plot out data, and also plot the model, and also spit back out the residual
+    ages = smoker_df.age
+    target = smoker_df.charges
+    prediction3 = test(x2, w, b)
+    plt.plot(smoker_df.age, predictions3, 'r', alpha=0.9);
+    plt.scatter(smoker_df.age, targets3, s=8, alpha=0.8);
+    plt.xlabel('Age');
+    plt.ylabel('Charges')
+    plt.legend(['Prediction', 'Actual']);
+
+try_parameters3(sgdr.coef_, sgdr.intercept_)
+
+#machine learning is simply computing the best paremeters to model a relationship between some feature and 
+
+#1) model, #cost function, #optimizer
+
+
+#using linear regression with multiple features, in this try we will BMI into linear regression model. 
+inputs, targets = non_smoker_df[['age', 'bmi']], non_smoker_df['charges'] 
+#all we did here was just added 'bmi' in the input variable, as a weight in a linear relationship, everything else stays the same
+
+
+
+model = LinearRegression().fit(inputs, targets) #you can immdieatly add the .fit to the model
+
+predictions = model.predict(inputs)
+                        
+
+                        loss = rmse(targets, predictions) #we can print the loss right away too
+print('Loss:', loss) #we can see bmi does not change the cost very much
+
+model.coef_, model.intercept_ #the weight and bias didnt change much from just age
+
+inputs, targets = acemedicaldf[['age', 'bmi', 'children']], acemedicaldf['charges'] #this is a model from the data with both smoker and nonesmoker
+
+
+
+    models2 = LinearRegression().fit(inputs, targets)
+
+predictions2 = models2.predict(inputs)
+
+loss = rmse(targets, predictions2) 
+print('Loss:', loss)
+
+#in some cases, we want to catorgize data, because with linear regression, we needed to use numbers. but with category we have none number valuyes
+#like with smoker/none smoker
+# 1) convert into binary (1,0) for 2 cats
+# 2) 'one hot', having multple columns, and if that data row has a 1 in one of the selected column than it is that value
+# 3) if you need to preseve the order of multple cats, (1,2,3,4) can still show in a linear relationship
+
+#here is the binary example with smoker and none smoker
+sns.barplot(data=acemedicaldf, x= 'smoker', y='charges')
+
+#making smoker data into binary
+smoker_codes = {'no': 0, 'yes': 1}
+acemedicaldf['smoker_code'] = acemedicaldf.smoker.map(smoker_codes)
+
+acemedicaldf #what happen here is we added a new column to the data, and to fill that column, we say, if the value in smoker is yes or no, fill the smoker code column with 0 or 1 based on the smoker colum
+
+acemedicaldf.charges.corr((acemedicaldf.smoker_code)) #if we look at the graph on top, we can see that there is a clear showing that smokers inluences the charges
+#this line of code confirms that smokers are closely correlated to charges
+
+inputs, targets = acemedicaldf[['age', 'bmi', 'children', 'smoker_code']], acemedicaldf['charges']
+
+model = LinearRegression().fit(inputs, targets)
+
+predictions1 = model.predict(inputs)
+
+loss = rmse(targets, predictions1) 
+print('Loss:', loss)
+#this data here shows thats smokers as a category is a significant influence on charges
+
+#now lets try with sex
+sex_code = {'male': 0, 'female': 1}
+acemedicaldf['sex_code'] = acemedicaldf.sex.map(sex_code)
+acemedicaldf
+
+inputs, targets = acemedicaldf[['age', 'bmi', 'children','smoker_code', 'sex_code']], acemedicaldf['charges']
+
+model11 = LinearRegression().fit(inputs, targets)
+
+prediction11 = model11.predict(inputs)
+loss = rmse(targets, prediction11) 
+print('Loss:', loss)
+
+#one hot encoding
