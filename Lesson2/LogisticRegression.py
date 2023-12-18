8f6abac49d0e9b0caac45e4f2d4a57e0ac99a6b0
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-12-18 16:41:08 -0700

    adding in data visualization

--- Lesson2/LogisticRegression.py
+++ Lesson2/LogisticRegression.py
@@ -32,3 +32,19 @@ raw_df = pd.read_csv(train_csv)
 # the number represent the probability of the input being 'Yes'
 #instead of RMSE, we use the cross entrophy loss function to evaluate the results
 #regression and classification are a part of supervised machine learning because they use labled data
+raw_df.info() #to check if there is any missing data
+#for 'rain today, and rain tomorrow, there is a lot of missing data. So we want to only use records that has rain today and tomorrow for our mode
+raw_df.dropna(subset=['RainToday', 'RainTomorrow'], inplace=True) #were dropping data where rain today and raintomorrow are empty
+raw_df.info() #here the total entries has dropped but rain today and rain tomorrow are equal
+#lets take a look at some data
+px.histogram(raw_df, x='Location', color='RainToday')
+px.scatter(raw_df.sample(2000), 
+           title='Min Temp. vs Max Temp.',
+           x='MinTemp', 
+           y='MaxTemp', 
+           color='RainToday')
+px.bar(raw_df.sample(2000), x='Location', y='Rainfall')
+px.histogram(raw_df, x='Temp3pm', color='RainTomorrow')
+px.histogram(raw_df, x='Temp9am', color='RainTomorrow')
+px.histogram(raw_df, x='RainToday', color='RainTomorrow') #this chart tells us for example, 92k instances that when it does not rain today, it also do not rain tomorrow
+#also an equal split where yes it rained today, but will or will not rain tomorrow. 
